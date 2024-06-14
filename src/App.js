a4b40240c36da61a668347c7fbb56161134a4ee1
carlos alberto gonzalez <carlos.gonzalez923@gmail.com>
2024-06-14 17:53:12 -0500

    terminacion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
-import { Context } from './context/Context';
-import { AppRouter } from './routing/AppRouter';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Context } from "./context/Context";
+import { AppRouter } from "./routing/AppRouter";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    console.log("useEffect lanzado por primera vez");
+    let user_local = JSON.parse(localStorage.getItem("user"));
+    setUser(user_local);
+  }, []);
+
+  useEffect(() => {
+    console.log("Este es un cambio de user");
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
   return (
     <div className="App">
-        <Context.Provider value='Inés María O.'>
-          <AppRouter />
-        </Context.Provider>
+      <Context.Provider value={{ user, setUser }}>
+        <AppRouter />
+      </Context.Provider>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { Context } from "../context/Context";
 export const Home = () => {
+  //? estructuracion
+  const { user } = useContext(Context);
   return (
     <div>
       <h1>Página de Inicio</h1>
-      <p>Hola Bienvenido a mi página de inicio</p>
+      <p>
+        Hola Bienvenid@ <strong>{user.name}</strong>, a mi tienda!!
+      </p>
+      <p>Phone: {user.phone}</p>
     </div>
-  )
-}
+  );
+};
